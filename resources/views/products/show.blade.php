a4fe8f7e0fc7709e972ddb5e6581419cd7ddab7f
daisan182 <sugisou9@gmail.com>
2022-07-11 04:30:22 +0000

    first commit

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,7 +5,11 @@
 <div class="d-flex justify-content-center">
     <div class="row w-75">
         <div class="col-5 offset-1">
+            @if ($product->image !== null)
+            <img src="{{ Storage::disk('s3')->url($product->image) }}" class="w-100 img-fluid">
+            @else
             <img src="{{ asset('img/dummy.png')}}" class="w-100 img-fuild">
+            @endif
         </div>
         <div class="col">
             <div class="d-flex flex-column">
@@ -27,6 +31,7 @@
                 <input type="hidden" name="id" value="{{$product->id}}">
                 <input type="hidden" name="name" value="{{$product->name}}">
                 <input type="hidden" name="price" value="{{$product->price}}">
+                <input type="hidden" name="carriage" value="{{$product->carriage_flag}}">
                 <div class="form-group row">
                     <label for="quantity" class="col-sm-2 col-form-label">数量</label>
                     <div class="col-sm-10">
@@ -66,25 +71,36 @@
 
         <div class="offset-1 col-10">
             <div class="row">
-                 @foreach($reviews as $review)
-                 <div class="offset-md-5 col-md-5">
-                     <p class="h3">{{$review->content}}</p>
-                     <label>{{$review->created_at}}</label>
-                 </div>
-                 @endforeach
-             </div>
+                @foreach($reviews as $review)
+                <div class="offset-md-5 col-md-5">
+                    <h3 class="review-score-color">{{ str_repeat('★', $review->score) }}</h3>
+                    <p class="h3">{{$review->content}}</p>
+                    <h4>{{$review->user->name}}</h4>
+                    <label>{{$review->created_at}}</label>
+                </div>
+                @endforeach
+            </div>
  
-             @auth
-             <div class="row">
-                 <div class="offset-md-5 col-md-5">
-                     <form method="POST" action="/products/{{ $product->id }}/reviews">
-                         {{ csrf_field() }}
-                         <textarea name="content" class="form-control m-2"></textarea>
-                         <button type="submit" class="btn samuraimart-submit-button ml-2">レビューを追加</button>
-                     </form>
-                 </div>
-             </div>
-             @endauth
+            @auth
+            <div class="row">
+                <div class="offset-md-5 col-md-5">
+                    <form method="POST" action="/products/{{ $product->id }}/reviews">
+                        {{ csrf_field() }}
+                        <h4>評価</h4>
+                        <select name="score" class="form-control m-2 review-score-color">
+                            <option value="5" class="review-score-color">★★★★★</option>
+                            <option value="4" class="review-score-color">★★★★</option>
+                            <option value="3" class="review-score-color">★★★</option>
+                            <option value="2" class="review-score-color">★★</option>
+                            <option value="1" class="review-score-color">★</option>
+                        </select>
+                        <h4>レビュー内容</h4>
+                        <textarea name="content" class="form-control m-2"></textarea>
+                        <button type="submit" class="btn samuraimart-submit-button ml-2">レビューを追加</button>
+                    </form>
+                </div>
+            </div>
+            @endauth
         </div>
     </div>
 </div>
